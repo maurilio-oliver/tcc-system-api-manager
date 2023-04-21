b870bbd9bf1280ecf246e4995504d7e2d2a9cf51
maurilio-oliver <maurilio1.5@hotmail.com>
2023-04-21 15:50:58 -0300

    first commit

---
+++ src/main/java/br/unip/tcc/tccapi/TccApiApplication.java
@@ -0,0 +1,13 @@
+package br.unip.tcc.tccapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TccApiApplication.class, args);
+    }
+
+}
